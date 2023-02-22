da5156cc9399667491e6a20b979aa0a2d080ab67
lipo <85422792+thiliporace@users.noreply.github.com>
2023-02-22 04:37:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyRun : MonoBehaviour
+{
+    [SerializeField] private Rigidbody enemy;
+    [SerializeField] private Transform player;
+    [SerializeField] private float distance = 0f;
+    [SerializeField] private float force = 2f;
+
+
+    private void Awake()
+    {
+        enemy = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+  
+    void Update()
+    {
+        RunFromPlayer();
+    }
+
+    void RunFromPlayer()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < distance)
+        {
+            enemy.AddForce(new Vector3(0,0,force));
+            //Debug.Log(force);
+        }
+
+    }
+}
